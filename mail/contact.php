04ab50792f3dd9ce6685374688d9a34ad0bba1c3
IronHeart123 <markandrei45@gmail.com>
2025-01-03 15:43:15 +0800

    eyyy

--- mail/contact.php
+++ mail/contact.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 // header("Access-Control-Allow-Methods: POST, GET, OPTIONS");  // Allow specific methods
-header("Access-Control-Allow-Headers: Content-Type");  // Allow specific headers
+header("Access-Control-Allow-Headers: *");  // Allow specific headers
 
 if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
